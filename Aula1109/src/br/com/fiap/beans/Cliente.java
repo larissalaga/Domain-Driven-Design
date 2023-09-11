5ea1b41bd2124eeee824037815468062290a0786
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-11 11:02:58 -0300

    Aula 11-09

---
+++ Aula1109/src/br/com/fiap/beans/Cliente.java
@@ -0,0 +1,45 @@
+package br.com.fiap.beans;
+
+public class Cliente {
+	//Visivilidade, tipo de dado e variavel
+	
+	private String nome;
+	private String cpf;
+	private int idade;
+	private double valor;
+	private Endereco endereco;
+	
+	//Getters e Setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+}
---
+++ Aula1109/src/br/com/fiap/beans/Endereco.java
@@ -0,0 +1,55 @@
+package br.com.fiap.beans;
+
+public class Endereco {
+	
+	//Visibilidade, tipo de dado, varivel
+	
+	private String logradouro;
+	private String cep;
+	private String numero;
+	private String complemento;
+	private String municipio;
+	private String estado;
+	
+	//Setters e Getters
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	
+
+}
