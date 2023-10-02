a24e400e2ec009e74ca1d48e0553aa63f525095a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-02 11:33:00 -0300

    Alterações em pastas

---
+++ Aula0210/src/br/com/fiap/beans/Colaborador.java
@@ -0,0 +1,38 @@
+package br.com.fiap.beans;
+
+public class Colaborador {
+	
+	private String nome;
+	private String cargo;
+	private double valorHora;
+	private double qtdHora;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public double getValorHora() {
+		return valorHora;
+	}
+	public void setValorHora(double valorHora) {
+		this.valorHora = valorHora;
+	}
+	public double getQtdHora() {
+		return qtdHora;
+	}
+	public void setQtdHora(double qtdHora) {
+		this.qtdHora = qtdHora;
+	}
+	
+	
+
+
+	
+}
---
+++ Aula0210/src/br/com/fiap/main/TesteCliente.java
@@ -0,0 +1,49 @@
+package br.com.fiap.main;
+
+import javax.swing.JOptionPane;
+
+import br.com.fiap.beans.Cliente;
+import br.com.fiap.beans.Colaborador;
+import br.com.fiap.beans.Endereco;
+
+public class TesteCliente {
+
+	public static void main(String[] args) {
+		// Instanciar objetos
+		Cliente objCliente = new Cliente();
+		Endereco objEndereco = new Endereco();
+		Colaborador objColaborador = new Colaborador();
+		
+		//Entradas
+		//Cliente
+		objCliente.setNome(JOptionPane.showInputDialog("Digite o nome: "));
+		objCliente.setIdade(Integer.parseInt(JOptionPane.showInputDialog("Digite sua idade: ")));
+		
+		//Endereco
+		objEndereco.setLogradouro(JOptionPane.showInputDialog("DIgite o Logradouro: "));
+		objEndereco.setCep(JOptionPane.showInputDialog("CEP: "));
+		objEndereco.setNumero(Integer.parseInt(JOptionPane.showInputDialog("Número: ")));
+		objCliente.setEndereco(objEndereco);
+		  
+		//Colaborador
+		objColaborador.setNome(JOptionPane.showInputDialog("Digite o nome do colaborador"));
+		objColaborador.setCargo(JOptionPane.showInputDialog("Digite o cargo"));
+		objColaborador.setValorHora(Double.parseDouble(JOptionPane.showInputDialog
+				("Informe o valor da hora trabalhada")));
+		objColaborador.setQtdHora(Double.parseDouble(JOptionPane.showInputDialog
+				("Informe a quantidade de horas trabalhadas")));
+		//Saidas
+		System.out.println("Nome do cliente: " + objCliente.getNome() + 
+				"\nIdade: " + objCliente.getIdade() + 
+				"\nLogradouro: " +objCliente.getEndereco().getLogradouro() + 
+				"\nCEP: " + objCliente.getEndereco().getCep() + 
+				"\nNumero: " + objCliente.getEndereco().getNumero() + 
+				"\n\nINFORMAÇÕES DO COLABORADOR\n" + 
+				"Nome: " + objColaborador.getNome() +
+				"\nCargo: " + objColaborador.getCargo() + 
+				"\nValor da Hora: " + objColaborador.getValorHora() + 
+				"\nQuantidade de horas: " + objColaborador.getQtdHora());
+		
+	}
+
+}
