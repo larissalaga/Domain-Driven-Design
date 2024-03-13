486c72b10b7af9f46c20350ad4b2de475dd1b8ef
larissalaga <larissa.laga@gmail.com>
2024-03-13 09:45:37 -0300

    Aula 13/03/24

---
+++ aula130324/src/br/com/fiap/main/TesteCadastroProduto.java
@@ -0,0 +1,42 @@
+package br.com.fiap.main;
+
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import br.com.fiap.beans.Produto;
+import br.com.fiap.dao.ProdutoDAO;
+
+public class TesteCadastroProduto {
+	// String
+	static String texto(String j) {
+		return JOptionPane.showInputDialog(j);
+	}
+
+	// Inteiro
+	static int inteiro(String j) {
+		return Integer.parseInt(JOptionPane.showInputDialog(j));
+	}
+
+	// Double
+	static double real(String j) {
+		return Double.parseDouble(JOptionPane.showInputDialog(j));
+	}
+public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	// TODO Auto-generated method stub
+	ProdutoDAO dao = new ProdutoDAO();
+	Produto objProduto = new Produto();
+	
+	objProduto.setCodigo(inteiro("Código"));
+	objProduto.setTipo(texto("Tipo"));
+	objProduto.setMarca(texto("Marca"));
+	objProduto.setValor(real("Valor"));
+	
+	System.out.println(dao.inserir(objProduto));
+	
+	
+		S
+
+	}
+
+}
